f84e8c4fea4c6f27f2e917654c9a63498b011294
orlandopena11 <orlandopena11@gmail.com>
2026-07-22 00:08:23 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,10 @@
-
+// Servidor de Inyección Directa para Supabase Client Auth
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.supabase = {}));
+})(this, (function (exports) { 'use strict';
+    var _supabaseJs = require('@supabase/supabase-js');
+    exports.createClient = _supabaseJs.createClient;
+    Object.defineProperty(exports, '__esModule', { value: true });
+}));
