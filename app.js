89665e94b9ad537fe26cb24d5d46ae80f69b7d72
orlandopena11 <orlandopena11@gmail.com>
2026-08-19 16:31:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -505,7 +505,7 @@ function renderizarMapaZillow()
         pAdicionales.style.fontSize = '11px';
         pAdicionales.style.color = '#64748b';
         pAdicionales.style.marginTop = '2px';
-        pAdicionales.textContent = `${propiedad.subtipoPropiedad} | Estacionamientos: ${propiedad.estacionamientos} | Año: ${propiedad.ano_construccion} | Estado: ${propiedad.estado_propiedad}`;
+        pAdicionales.textContent = `${prop.subtipoPropiedad} | Estacionamientos: ${prop.estacionamientos} | Año: ${prop.ano_construccion} | Estado: ${prop.estado_propiedad}`;
      
         //  AGREGAR AQUÍ: Inyección del título / dirección de la propiedad
         const pDireccion = document.createElement('div');
