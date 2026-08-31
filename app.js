bb2238d81b2bfc719e350eed8035cefc29285a8d
orlandopena11 <orlandopena11@gmail.com>
2026-08-30 21:50:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2538,4 +2538,6 @@ function iniciarSesionSocialSupabase(proveedorOAuth) { // --> [Abre la función 
 // Vinculación explícita global superior
 window.ejecutarAutenticacionTunelRestSRE = ejecutarAutenticacionTunelRestSRE;
 window.iniciarSesionSocialSupabase = iniciarSesionSocialSupabase;
+    
+} // <-- SRE FIX: Cerramos la llave que se quedó abierta desde la línea 815
 // =========================================================================
