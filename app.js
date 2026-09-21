0f56e95ef6f94c05374f8e962f494501aa077b0f
orlandopena11 <orlandopena11@gmail.com>
2026-09-20 21:55:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1475,7 +1475,8 @@ function inyectarDatosPropiedadAlMensaje() { // Inicia inyectarDatosPropiedadAlM
         // ==========================================================================
         
         // Inyectar dinámicamente el botón de Ver Teléfono en el panel comercial de la Cortina SPA
-        const contenedorFijoFicha = document.querySelector('.columna-informacion-fija, style*="position: sticky"');
+        const contenedorFijoFicha = document.querySelector('.columna-informacion-fija') || document.getElementById('btn-solicitar-tour-galeria')?.parentNode;
+
         let btnTelefonoSRE = document.getElementById('btn-ver-telefono-premium');
         
         if (!btnTelefonoSRE && contenedorFijoFicha) {
